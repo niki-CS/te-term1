5d2f558a1a6cca799a35716be16a7537638d695e
Nicol Gil <nicole.gil@csedge.org>
2022-11-05 15:39:18 -0400

    berry falls

--- catchem.py/berry.py
+++ catchem.py/berry.py
@@ -7,19 +7,22 @@ sense = SenseHat()
 class Berry:
     def __init__(self,color,speed,value):
         self.color = color
-        self.speed = speed
+        self.speed = .5
         self.position = random.randint(0,7)
         self.value = value
+        self.pos_X = random.randint(0,7)
+        self.pos_Y = 0
 
     def drop(self):
-        if self.position <= 55:
-            self.position += 8
-            sleep(self.speed)
-            self.display()        
+        sleep(self.speed)
+        if self.pos_Y < 7:
+            self.pos_Y += 1  
+        self.display()     
     
     def display(self):
-        x = self.position%8
-        y = self.position//8
+        x = self.pos_X
+        y = self.pos_Y
         sense.set_pixel(x, y-1, (0,0,0))
-        sense.set_pixel(x, y, self.color)
+        sense.set_pixel(x, y, (255,0,0))
+        #print("berry moved")
         
--- catchem.py/game.py
+++ catchem.py/game.py
@@ -5,13 +5,14 @@ from player import Player
 from berry import Berry
 
 sense = SenseHat()
+points = 0
 
 class Game:
     def __init__(self):
         self.score = 0
         self.game_over = False
         self.speed = .5
-        self.berries = []
+        self.berry = Berry(0,7,sense)
         self.player = Player(56,63,sense)
 
     def start(self):
@@ -19,11 +20,19 @@ class Game:
     
     def play(self):
         self.start()
-        self.player.display(0)
-        self.berry.display()
         while not self.game_over:
+            self.player.display(0)
+            self.berry.drop()
             for event in sense.stick.get_events():
                 if event.action == "pressed" and event.direction == "left":
                     self.player.move_left()
                 elif event.action == "pressed" and event.direction == "right":
                     self.player.move_right()
+           # if self.player.position == self.berry.pos_X:
+           #     points += 1
+           #     self.berry.drop()
+                #clear berry
+           # if self.player.position != self.berry.pos_Y:
+           #     sense.show_message(f"Game Over. Points: {points}", text_colour = (255,0,0), scroll_speed= 0.05)
+           #     self.game_over = True
+           #     active = False
